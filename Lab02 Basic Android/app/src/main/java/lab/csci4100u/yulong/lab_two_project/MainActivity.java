4412a6462f392a45a600b175815021502420a239
jocstech <jocstech@gmail.com>
2016-11-18 13:58:30 -0500

    Initial Addition

---
+++ Lab02 Basic Android/app/src/main/java/lab/csci4100u/yulong/lab_two_project/MainActivity.java
@@ -0,0 +1,14 @@
+package lab.csci4100u.yulong.lab_two_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout); // set to new layout screen here.
+    }
+}
