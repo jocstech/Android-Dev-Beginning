c0959b8febe984aedbc8b7eb9aa71fef7a917d78
jocstech <jocstech@gmail.com>
2016-11-18 18:07:32 -0500

    lab09

---
+++ Lab09 Maps/app/src/main/java/csci4100/uoit/ca/lab09maps/ShowLocation.java
@@ -0,0 +1,28 @@
+package csci4100.uoit.ca.lab09maps;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ShowLocation extends AppCompatActivity implements OnMapReadyCallback  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_activity);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+}
